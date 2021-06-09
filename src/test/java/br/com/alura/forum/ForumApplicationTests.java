6f6f204455289fd0cae41637f2f1923ea36665c9
Mikaela Soares <miih.arj@gmail.com>
2021-06-09 11:56:22 -0300

    Testes automatizados

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,13 +1,21 @@
 package br.com.alura.forum;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class ForumApplicationTests {
+public class ForumApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
